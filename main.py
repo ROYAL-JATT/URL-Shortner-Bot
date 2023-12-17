b9dafc771d3e431140b33d37ce76d413203f9278
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-12-17 09:34:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client
 
-bot_token = os.environ["6372645892:AAGCSvh6mDAQuzu01OW4S-k9G9mAJgJYH4w"]
+bot_token = os.environ.get("6372645892:AAGCSvh6mDAQuzu01OW4S-k9G9mAJgJYH4w")
 api_id = int(os.environ["28280795"])
 api_hash = os.environ["3f02c2df6069de8c1a2abf623da0a4b8"]
 plugins = dict(
